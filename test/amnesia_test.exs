21bde3c702b1ace26d228eeacc0bf6ee844b5669
meh <meh@schizofreni.co>
2013-04-10 22:00:33 +0200

    test: make it more meaningful

--- test/amnesia_test.exs
+++ test/amnesia_test.exs
@@ -5,32 +5,57 @@ defmodule AmnesiaTest do
   use Amnesia
 
   defdatabase Database do
-    deftable Message, [:user, :message], type: :bag do
-      @type t :: __MODULE__[user: integer, message: String.t]
+    deftable Message, [:user_id, :content], type: :bag do
+      @type t :: __MODULE__[user_id: integer, content: String.t]
 
-      def bar do
-        42
+      def user(self) do
+        User.read(self.user_id)
+      end
+
+      def user!(self) do
+        User.read!(self.user_id)
       end
     end
 
     deftable User, [:id, :name, :email] do
       @type t :: __MODULE__[id: integer, name: String.t, email: String.t]
+
+      def add_message(content, self) do
+        Message[user_id: self.id, content: content].write
+      end
+
+      def add_message!(content, self) do
+        Message[user_id: self.id, content: content].write!
+      end
+
+      def messages(self) do
+        Message.read(self.id)
+      end
+
+      def messages!(self) do
+        Message.read!(self.id)
+      end
     end
   end
 
   test "type checking works" do
-    assert Database.Message.bag?
     assert Database.User.set?
+    assert Database.Message.bag?
   end
 
   test "saves item" do
     transaction! do
-      Database.Message[user: 23, message: "yo dawg"].write
+      user = Database.User[id: 23]
+      user.add_message("yo dawg")
+      user.write
     end
 
     assert(transaction! do
-      Database.Message.read(23)
-    end == { :atomic, [Database.Message[user: 23, message: "yo dawg"]] })
+      assert Database.User.read(23) == Database.User[id: 23]
+      assert Database.User.read(23).messages == [Database.Message[user_id: 23, content: "yo dawg"]]
+
+      :ok
+    end == { :atomic, :ok })
   end
 
   setup_all do

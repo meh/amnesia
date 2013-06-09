41f9b61091d7b9682d971e36933c54d2ef336bb1
meh <meh@schizofreni.co>
2013-06-09 17:54:00 +0200

    package: update version

--- package.exs
+++ package.exs
@@ -1,9 +1,9 @@
 Expm.Package.new(
   name: "amnesia",
   description: "Wrapper for mnesia",
-  version: "0.0.1",
+  version: "0.0.2",
   licenses: [[name: "WTFPL"]],
   keywords: ["mnesia", "database"],
   maintainers: [[name: "meh.", email: "meh@schizofreni.co"]],
-  repositories: [[github: "meh/amnesia", tag: "v0.0.1"]]
+  repositories: [[github: "meh/amnesia", tag: "v0.0.2"]]
 )

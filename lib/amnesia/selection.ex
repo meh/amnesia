367e6c55ad1c47d4b2aaa4364940af01f5bce98d
meh <meh@schizofreni.co>
2014-07-04 19:36:25 +0200

    selection: implement for nil as well

--- lib/amnesia/selection.ex
+++ lib/amnesia/selection.ex
@@ -3,3 +3,17 @@ defprotocol Amnesia.Selection do
   def next(self)
   def values(self)
 end
+
+defimpl Amnesia.Selection, for: Atom do
+  def coerce(nil, _) do
+    nil
+  end
+
+  def next(nil) do
+    nil
+  end
+
+  def values(nil) do
+    []
+  end
+end

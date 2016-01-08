63f6aef1888b15669f80b5e06af32d151f23e4ff
meh <meh@schizofreni.co>
2016-01-08 22:02:16 +0100

    mix: bump version and requirements

--- mix.exs
+++ mix.exs
@@ -3,15 +3,15 @@ defmodule Amnesia.Mixfile do
 
   def project do
     [ app: :amnesia,
-      version: "0.2.0",
-      elixir: "~> 1.0.0-rc1 or ~> 1.1.0-rc.0",
+      version: "0.2.1",
+      elixir: "~> 1.0.0-rc1 or ~> 1.1.0-rc.0 or ~> 1.2.0-rc.0",
       deps: deps,
       package: package,
       description: "mnesia wrapper for Elixir" ]
   end
 
   defp package do
-    [ contributors: ["meh"],
+    [ maintainers: ["meh"],
       licenses: ["WTFPL"],
       links: %{"GitHub" => "https://github.com/meh/amnesia"} ]
   end
@@ -21,6 +21,6 @@ defmodule Amnesia.Mixfile do
   end
 
   defp deps do
-    [ { :exquisite, "~> 0.1.4" } ]
+    [ { :exquisite, "~> 0.1.6" } ]
   end
 end

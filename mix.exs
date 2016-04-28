5e6424aba0a2a837cae874d05d55089e92767296
meh <meh@schizofreni.co>
2016-04-28 19:24:31 +0200

    mix: bump version

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Amnesia.Mixfile do
 
   def project do
     [ app: :amnesia,
-      version: "0.2.1",
+      version: "0.2.2",
       elixir: "~> 1.0.0-rc1 or ~> 1.1.0-rc.0 or ~> 1.2.0-rc.0",
       deps: deps,
       package: package,
--- package.exs
+++
@@ -1,9 +0,0 @@
-Expm.Package.new(
-  name: "amnesia",
-  description: "Wrapper for mnesia",
-  version: "0.2.0",
-  licenses: [[name: "WTFPL"]],
-  keywords: ["mnesia", "database"],
-  maintainers: [[name: "meh.", email: "meh@schizofreni.co"]],
-  repositories: [[github: "meh/amnesia", tag: "v0.2.0"]]
-)

3ad6dc514de8e289478cc5244efbf9d1e747d24b
Vadim Tsvetkov <florius0@ninsar.pro>
2022-06-19 01:22:18 +0300

    Fix typo

--- .formatter.exs
+++ .formatter.exs
@@ -1,12 +1,12 @@
 [
   inputs: ["*.{ex,exs}", "{config,lib,test}/**/*.{ex,exs}"],
   locals_without_parens: [
-    deftable: :*
+    deftable: :*,
     defhook: :*
   ],
   export: [
     locals_without_parens: [
-      deftable: :*
+      deftable: :*,
       defhook: :*
     ]
   ]
